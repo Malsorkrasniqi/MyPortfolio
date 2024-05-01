0062c0f85ea34aa324183d380ae7a01fec27fd6f
Malsor <Malsorkrasniqi60@gmail.com>
2024-05-01 13:33:53 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1020px",
+        xl: "1440px",
+      },
+      fontFamily: {
+        "sans": ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
